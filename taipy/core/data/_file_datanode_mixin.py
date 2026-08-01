74c840c16ea65023d85e85d28c585488d119425d
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2026-08-01 11:42:37 +0000

    137

--- taipy/core/data/_file_datanode_mixin.py
+++ taipy/core/data/_file_datanode_mixin.py
@@ -48,8 +48,8 @@ class _FileDataNodeMixin:
 
     def __init__(self, properties: Dict) -> None:
         initial_path = properties.get(self._PATH_KEY, properties.get(self._DEFAULT_PATH_KEY))
-        self._path: Optional[str] = initial_path if isinstance(initial_path, str) else None
-        self._is_generated: bool = properties.get(self._IS_GENERATED_KEY, self._path is None)
+        self._path: str = initial_path if isinstance(initial_path, str) else ""
+        self._is_generated: bool = properties.get(self._IS_GENERATED_KEY, initial_path is None)
         self._last_edit_date: Optional[datetime] = None
 
         if self._path and ".data" in self._path:
