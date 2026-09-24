d2cb4431434ab81f571f8908c1608502b8cc5add
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-24 18:38:19 -0400

    Muna4029__diff__id_134

--- taipy/core/data/excel.py
+++ taipy/core/data/excel.py
@@ -177,7 +177,9 @@ class ExcelDataNode(DataNode, _FileDataNodeMixin, _TabularDataNodeMixin):
             sheet_names = excel_file.sheetnames
 
             user_provided_sheet_names = properties.get(self.__SHEET_NAME_PROPERTY) or []
-            if not isinstance(user_provided_sheet_names, (list, set, tuple)):
+            if isinstance(user_provided_sheet_names, set):
+                user_provided_sheet_names = list(user_provided_sheet_names)
+            elif not isinstance(user_provided_sheet_names, (list, tuple)):
                 user_provided_sheet_names = [user_provided_sheet_names]
 
             provided_sheet_names = user_provided_sheet_names or sheet_names
@@ -263,7 +265,9 @@ class ExcelDataNode(DataNode, _FileDataNodeMixin, _TabularDataNodeMixin):
 
         with pd.ExcelWriter(self._path, mode="a", engine="openpyxl", if_sheet_exists="overlay") as writer:
             if sheet_name:
-                if not isinstance(sheet_name, str):
+                if isinstance(sheet_name, set):
+                    sheet_name = list(sheet_name)[0]
+                elif not isinstance(sheet_name, str):
                     sheet_name = sheet_name[0]
                 append_excel_fct(
                     writer, *args, **kwargs, sheet_name=sheet_name, startrow=writer.sheets[sheet_name].max_row
